e48fc4f37479302f2e6869e38fa182c9e231f803
Sabina321 <sabina.tomkins@gmail.com>
2015-03-06 13:17:04 -0800

    linear regression nb

--- essential_game_functionality.py
+++ essential_game_functionality.py
@@ -13,8 +13,20 @@ def average_fitness_at_x(w,x):
         w_bar = w_bar+np.dot(x[i],strategy_fitness_at_x(w,i,x))
     return w_bar
 
+def discrete_replicator(s,w,i):
+    return s[i] * (strategy_fitness_at_x(w,i,s)/average_fitness_at_x(w,s))
 
-
+def get_delta_line_two(w,i,j):
+    si,sj=symbols('si,sj')
+    a = [si,sj]
+    if j==0:
+        a = [1-sj,sj]
+    elif j==1:
+        a = [si,1-si]
+    wi = np.dot(a,w[i])
+    wj = np.dot(a,w[j])
+    z = wi-wj
+    return [diff(z,si),diff(z,sj),[x if type(x)==Integer else 0 for x in z.args][0]]
 
 def get_delta_line(w,i,j,k):
     si,sj,sk=symbols('si,sj,sk')
@@ -41,4 +53,10 @@ def get_interior_equilibrium_3_by_3(w):
         return final_shares
     else:
         return "No internal equilibrium"
+
+def get_interior_equilibrium_two_by_two(w):
+    delta_w12=get_delta_line_two(w,0,1)
+    si,sj=symbols('si,sj')
+    return solve(delta_w12[0]*si+delta_w12[1]*sj+delta_w12[2])
+
 
